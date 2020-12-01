e54632add9792e876b3bc79b97f6175fe665da1c
Angel Macias <a320670@uach.mx>
2020-12-01 11:31:07 -0700

    deteting unwanted files

--- models/about.js
+++
@@ -1,7 +0,0 @@
-class About {
-
-servidor = "Celesital Resort"
-
-equipo = [{"name":["Alegandro","Sebastián","Victor Hugo","Angel Eidu"],"lastName":["Aguirre Baeza","Terrazas Sandoval","Aguilar Parra","Macias Saenz"]}]
-
-}
--- proyecto/app.js
+++ proyecto/app.js
@@ -1,4 +1,7 @@
+const { response } = require('express');
 const express = require('express');
+const { request } = require('http');
+const { render, renderFile } = require('pug');
 const app = express();
 
 app.use(express.static(__dirname + '/public/'));
@@ -8,3 +11,29 @@ app.listen('3000', function() {
 });
 
 app.use(express.static(__dirname + '/public/'));
+
+var url = require('url')
+
+module.exports = {
+  handleRequest: function(req, res){
+    response.writeHead(200, {'Content-Type': 'text/html'})
+  
+    var path = url.parse(request.url).pathname
+    switch (path) {
+      case '/index':
+        renderFile('./index.html', response)
+        break
+      case '/team':
+        renderFile('./team.html',response)
+        break
+      case '/about':
+        renderFile('./about',response)
+        break
+      default:
+        response.writeHead('404')
+        response.write('Fallo de Routing')
+        response.end;
+    }
+
+  }
+}
