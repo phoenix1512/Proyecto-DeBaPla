d2e4032cddcbd3da2f263d7c904bd7e757eba96e
Angel Macias <a320670@uach.mx>
2020-12-01 21:32:56 -0700

    la ruta funciona

--- proyecto/about.js
+++ proyecto/about.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/columns')
-
-router.get('/', controller.list);
-
+const about = require('./about.json')
+const fs = require('fs');
+const { response } = require('express');
 
 module.exports = router;
--- proyecto/app.js
+++ proyecto/app.js
@@ -1,17 +1,17 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const express = require('express');
 const { request } = require('http');
 const { render, renderFile } = require('pug');
 const app = express();
 const about = require('./about.json');
 
-app.use('/about', about);
+app.use('/about', function (req, res) {
+  res.json(about); 
+})
 
 app.use(express.static(__dirname + '/public/'));
 
 app.listen('3000', function() {
   console.log('Servidor web escuchando en el puerto 3000');
 });
-
-app.use(express.static(__dirname + '/public/'));
 
